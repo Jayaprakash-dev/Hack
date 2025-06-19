766627b884f2bdd606e02187b0336e91f92ce92f
jayaprakash-dev <jayaprakash07.dev@gmail.com>
2025-06-20 00:06:18 +0530

    refactor Fill assembly code logic

--- Assembly/Fill/Fill.asm
+++ Assembly/Fill/Fill.asm
@@ -1,98 +1,168 @@
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
-// the screen should be cleared.
+//// Runs an infinite loop that listens to the keyboard input. 
+//// When a key is pressed (any key), the program blackens the screen,
+//// i.e. writes "black" in every pixel. When no key is pressed, 
+//// the screen should be cleared.
+//
+//@SCREEN
+//D = A // screen mapper base address
+//
+//// Screen = 256×512 = 131072 pixels = 8192 words (16 bits each)
+//// Screen base = 16384 -> end = (16384 + 8192) - 1 = 24575
+//// (Subtract 1 due to 0-based addressing)
+//
+//@24575
+//D = A // end address of screen mapper
+//@len
+//M = D
+//M = M + 1 // length of screen mapper
+//
+//@RESET_FLAG
+//M = 0 // Track screen state (0 = cleared, 1 = black)
+//
+//(LOOP)
+//    @KBD
+//    D = M // read keyboard input
+//    @RESET_SCREEN
+//    D; JEQ // If no key is pressed, clear the screen
+//
+//    @RESET_FLAG
+//    D = M
+//    @LOOP
+//    D; JGT // If screen is already black, skip
+//
+//    // Set i = base address of screen
+//    @SCREEN
+//    D = A // screen mapper base memory address
+//    @i
+//    M = D
+//
+//    (BLACKEN_SCREEN) // sets every word in screen mapper to -1
+//        @i
+//        D = M
+//        @len
+//        D = M - D // D = RAM[len] - RAM[i]
+//        @BLACKEN_SCREEN_DONE
+//        D; JEQ // if D == 0 then END
+//
+//        @i
+//        A = M
+//        M = -1 // RAM[A] = -1
+//
+//        @i
+//        M = M + 1 // i++
+//
+//        @BLACKEN_SCREEN
+//        0; JMP
+//
+//    (BLACKEN_SCREEN_DONE)
+//        @RESET_FLAG
+//        M = 1
+//        @LOOP
+//        0; JMP
+//
+//    (RESET_SCREEN) // sets every word in screen mapper to 0
+//        @RESET_FLAG
+//        D = M
+//        @LOOP
+//        D; JEQ // if RESET_FLAG == 0 then goto LOOP (no need to reset screen)
+//
+//        // set i = base add of screen mapper
+//        @SCREEN
+//        D = A
+//        @i
+//        M = D
+//
+//        (RESET_SCREEN_LOOP)
+//            @i
+//            D = M
+//            @len
+//            D = M - D // D = RAM[len] - RAM[i]
+//            @RESET_DONE
+//            D; JEQ // if D == 0 then END
+//
+//            @i
+//            A = M
+//            M = 0 // RAM[A] = 0
+//
+//            @i
+//            M = M + 1 // i++
+//
+//            @RESET_SCREEN_LOOP
+//            0; JMP
+//        
+//        (RESET_DONE)
+//            @RESET_FLAG
+//            M = 0
+//            @LOOP
+//            0; JMP
+//
 
-@SCREEN
-D = A // screen mapper base address
-
-// Screen = 256×512 = 131072 pixels = 8192 words (16 bits each)
-// Screen base = 16384 -> end = (16384 + 8192) - 1 = 24575
-// (Subtract 1 due to 0-based addressing)
-
-@24575
-D = A // end address of screen mapper
-@len
-M = D
-@len
-M = M + 1 // length of screen mapper
-
-@RESET_FLAG
-M = 0 // Track screen state (0 = cleared, 1 = black)
+@SCREEN_COLOR
+M = 0
 
 (LOOP)
-    @KBD
-    D = M // read keyboard input
-    @RESET_SCREEN
-    D; JEQ // If no key is pressed, clear the screen
+@SCREEN
+D = A
 
-    @RESET_FLAG
+@CURRENT_PIXEL
+M = D
+
+@KBD
+D = M
+
+@BLACKEN_SCREEN
+D; JGT
+
+@WHITEN_SCREEN
+D; JEQ
+
+(BLACKEN_SCREEN)
+    @SCREEN_COLOR
     D = M
     @LOOP
-    D; JGT // If screen is already black, skip
+    D; JGT // if SCREEN_COLOR == 1 then screen is already black, skip
 
-    // Set i = base address of screen
-    @SCREEN
-    D = A // screen mapper base memory address
-    @i
-    M = D
+    @SCREEN_COLOR
+    M = 1
+    
+    @SCREEN_FILL_VALUE
+    M = -1
+    @CHANGE_SCREEN_PIXELS
+    0; JMP
 
-    (BLACKEN_SCREEN) // sets every word in screen mapper to -1
-        @i
-        D = M
-        @len
-        D = M - D // D = RAM[len] - RAM[i]
-        @BLACKEN_SCREEN_DONE
-        D; JEQ // if D == 0 then END
+(WHITEN_SCREEN)
+    @SCREEN_COLOR
+    D = M
+    @LOOP
+    D; JEQ // if SCREEN_COLOR == 0 then screen is already white, skip
 
-        @i
-        A = M
-        M = -1 // RAM[A] = -1
+    @SCREEN_COLOR
+    M = 0
 
-        @i
-        M = M + 1 // i++
+    @SCREEN_FILL_VALUE
+    M = 0
+    @CHANGE_SCREEN_PIXELS
+    0; JMP
 
-        @BLACKEN_SCREEN
-        0; JMP
+(CHANGE_SCREEN_PIXELS)
+    @SCREEN_FILL_VALUE
+    D = M
 
-    (BLACKEN_SCREEN_DONE)
-        @RESET_FLAG
-        M = 1
-        @LOOP
-        0; JMP
+    @CURRENT_PIXEL
+    A = M
+    M = D // get RAM[A] and fill it with the -1 or 0
 
-    (RESET_SCREEN) // sets every word in screen mapper to 0
-        @RESET_FLAG
-        D = M
-        @LOOP
-        D; JEQ // if RESET_FLAG == 0 then goto LOOP (no need to reset screen)
+    @CURRENT_PIXEL
+    D = M + 1
 
-        // set i = base add of screen mapper
-        @SCREEN
-        D = A
-        @i
-        M = D
+    @KBD
+    D = A - D
 
-        (RESET_SCREEN_LOOP)
-            @i
-            D = M
-            @len
-            D = M - D // D = RAM[len] - RAM[i]
-            @RESET_DONE
-            D; JEQ // if D == 0 then END
+    @CURRENT_PIXEL
+    M = M + 1
 
-            @i
-            A = M
-            M = 0 // RAM[A] = 0
+    @CHANGE_SCREEN_PIXELS
+    D; JGT
 
-            @i
-            M = M + 1 // i++
-
-            @RESET_SCREEN_LOOP
-            0; JMP
-        
-        (RESET_DONE)
-            @RESET_FLAG
-            M = 0
-            @LOOP
-            0; JMP
+@LOOP
+0; JMP
